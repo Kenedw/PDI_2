b78f6fa6ca252bbf215e2496d0778fbeeaa7988e
Jordy Allyson <jordy.sousa@eng.ci.ufpb.br>
2019-04-23 21:17:07 -0400

    Tentativa de plotar a frequencia em cada ponto

--- parte3.py
+++ parte3.py
@@ -1,9 +1,10 @@
 #required libraries
-from urllib.request import urlretrieve
 import scipy.io.wavfile
 import pydub
-from numpy import fft as fft
+from numpy import fft as fft 
+import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt2 
 
 #a temp folder for downloads
 temp_folder="/Users/LMI/Documents/PDI/"
@@ -13,15 +14,34 @@ rate,audData=scipy.io.wavfile.read(temp_folder+"audio.wav")
 print(rate)     #Samples per seconds of the signal
 print(audData)  #Data of the signal
 
-print(audData.shape[0] / rate) #length of the signal
+print(audData.shape[0] / rate ) #length of the signal
 
-channel1 = audData[:,0]     #channel of the left
-channel2 = audData[:,1]     #channel of the right
+length = audData.shape[0] // rate #length of the audio signal
 
-fourier = fft.fft(audData)
+#channel1 = audData[:,0]     #channel of the left
+#channel2 = audData[:,1]     #channel of the right
 
-plt.plot(fourier, color='#ff7f00')
+four = fft.fft(audData)
+#four.astype('complex')
+
+plt.plot(four.real)
 plt.xlabel('K')
 plt.ylabel('Amplitude')
+plt.show()
 
+four = four[0:(length//2)]
+#four = four/float(length)
+
+#Calculates the frequency at each point in Hz
+each_freq = np.arange(0, (length/2), 1.0) * (rate)
+
+plt2.plot(each_freq/1000, 10*np.log10(four))
+plt2.xlabel("Frequencia (kHz)")
+plt2.ylabel("Potência (dB)")
+plt2.show()
+
+
+
+    
+    
 
